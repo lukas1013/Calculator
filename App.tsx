d5aac515ce1676ced22445422b74c0aa00051fa3
lukas1013 <leonardolukas10@gmail.com>
2021-10-07 10:22:12 -0300

    init

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Calculator</Text>
+    </View>
+  )
+};
+
+export default App;
